df026d049a6b16530a5458c6aa5667d4f18e19ea
h_ooe <h_ooe@PRISM01>
2023-06-01 18:13:08 +0900

    プロジェクト ファイルを追加します。

---
+++ SerialTest/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace SerialTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "通信開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(173, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 55);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "LED ON/OFF";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(130, 90);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 52);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "DARK-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(239, 90);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 52);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "BRIGHT+";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(374, 93);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(109, 48);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "終了";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 154);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "LEDコントローラー";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ SerialTest/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace SerialTest
+{
+    public partial class Form1 : Form
+    {
+        private bool commStart = false;
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (commStart == false)
+            {
+                serialPort1.BaudRate = 9600;
+                serialPort1.Parity = Parity.None;
+                serialPort1.DataBits = 8;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.Handshake = Handshake.None;
+                serialPort1.PortName = "COM3";
+                try
+                {
+                    serialPort1.Open();
+                    serialPort1.DiscardInBuffer();
+                    serialPort1.DiscardOutBuffer();
+
+                    commStart = true;
+                    button1.Text = "通信停止";
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("COMポートが使用できません！");
+                    //                    throw;
+                    return;
+                }
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+            }
+            else
+            {
+                serialPort1.DiscardInBuffer();
+                serialPort1.DiscardOutBuffer();
+                serialPort1.Close();
+
+                commStart = false;
+                button1.Text = "通信開始";
+
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (commStart == true)
+                serialPort1.Write("L");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (commStart == true)
+                serialPort1.Write("B");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (commStart == true)
+                serialPort1.Write("D");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
